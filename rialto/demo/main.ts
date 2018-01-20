3e863890ec6e0fed9f9b5c45c2c88bbb8d2fd608
Stephan Hesse <tchakabam@gmail.com>
2018-01-20 02:00:25 +0100

    set up basic vue based demo scaffold

--- rialto/demo/main.ts
+++ rialto/demo/main.ts
@@ -1,14 +1,36 @@
 import {MediaSession} from '../index'
 
+import Vue from 'vue'
+
+const MainTemplate = require('./main.vue.html')
+
 export namespace RialtoDemoApp {
 
   class RialtoDemoApp {
+
+    _vueApp: Vue;
 
     constructor(videoElement) {
 
       const mediaSession = new MediaSession(videoElement,
         this. onMediaElementEventTranslatedCb_,
         this.onPlaybackStateMachineTransitionCb_);
+
+      Vue.component('main-component', {
+        template: MainTemplate,
+        data () {
+          return {
+            message: 'Hello'
+          }
+        }
+      })
+
+      // create a root instance
+      const app = new Vue({
+        el: '#app'
+      })
+
+      this._vueApp = app;
     }
 
     onPlaybackStateMachineTransitionCb_(mediaSession) {
