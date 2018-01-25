cde828d1d4e1a45613658fd76cdc772ab7ba5e76
Stephan Hesse <tchakabam@gmail.com>
2018-01-25 22:20:40 +0100

    media-locator extract byte-range

--- rialto/lib/media-locator.ts
+++ rialto/lib/media-locator.ts
@@ -1,31 +1,23 @@
+import {ByteRange} from './xhr'
+
 export type MediaClockTime = number
-
-export class ByteRange {
-
-    from: number;
-    to: number;
-
-    constructor(from, to) {
-      this.from = from
-      this.to = to
-    }
-}
 
 export class MediaLocator {
 
-    uri: string;
-    startTime: MediaClockTime;
-    endTime: MediaClockTime;
-    byteRange: ByteRange;
+  uri: string;
+  startTime: MediaClockTime;
+  endTime: MediaClockTime;
+  byteRange: ByteRange;
 
-    static get ByteRange() {
-      return ByteRange
-    }
-
-    constructor(uri, startTime, endTime, byteRange) {
-      this.uri = uri
-      this.startTime = startTime
-      this.endTime = endTime
-      this.byteRange = byteRange
-    }
+  constructor(
+    uri: string,
+    startTime: MediaClockTime = NaN,
+    endTime: MediaClockTime = NaN,
+    byteRange: ByteRange = null
+  ) {
+    this.uri = uri
+    this.startTime = startTime
+    this.endTime = endTime
+    this.byteRange = byteRange
+  }
 }
