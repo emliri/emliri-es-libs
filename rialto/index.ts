7862299da29707bf94aa555b35a219390ee52532
Stephan Hesse <tchakabam@gmail.com>
2018-01-25 22:22:03 +0100

    lib index extensions

--- rialto/index.ts
+++ rialto/index.ts
@@ -7,12 +7,21 @@ import {
 } from './lib/playback-state-machine'
 import {MediaElementObserver} from './lib/media-element-observer'
 import {MediaSession} from './lib/media-session'
+import {MediaSegment} from './lib/media-segment'
+import {MediaSegmentQueue} from './lib/media-segment-queue'
+import {MediaLocator} from './lib/media-locator'
+import {Logger, getLogger} from './lib/logger'
 
 import {Observable} from '../objec-ts/index'
 
 export {
+  Logger,
+  getLogger,
   MediaElementObserver,
   MediaSession,
+  MediaLocator,
+  MediaSegment,
+  MediaSegmentQueue,
   PlaybackStateMachine,
   PlaybackStateMachineTransitions,
   PlaybackStateMachineTransitionReasons,
